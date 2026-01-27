0916d812a1dcf00791ffd1b8f0d0f4ecb4576987
Ian Griffiths <icarillius@gmail.com>
2026-01-27 14:16:09 +0000

    Pre widget Start

--- enc_temp_folder/1a378263a45d7d6115893ff8ef558be/NanGame.Build.cs
+++ enc_temp_folder/1a378263a45d7d6115893ff8ef558be/NanGame.Build.cs
@@ -8,7 +8,7 @@ public class NanGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
